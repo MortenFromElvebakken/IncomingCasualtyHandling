be85c55053dcc4b4994426e5568344cce2489950
Anni Lykke Wilhelmsen <201509504@post.au.dk>
2018-11-05 15:05:19 +0100

    Added Triage and Specialty models

---
+++ IncomingCasualtyHandling/BL/Models/Specialty.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace IncomingCasualtyHandling.BL.Models
+{
+    public class Specialty
+    {
+        public string Name { get; set; }
+        public string Colour { get; set; }
+        public int Amount { get; set; }
+        public Visibility ShowAs { get; set; }
+   
+    }
+}
---
+++ IncomingCasualtyHandling/BL/Models/Triage.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace IncomingCasualtyHandling.BL.Models
+{
+    public class Triage
+    {
+        public string Name { get; set; }
+        public string Colour { get; set; }
+        public int Amount { get; set; }
+        public double Height { get; set; }
+        public Visibility ShowAs { get; set; }
+    }
+}
