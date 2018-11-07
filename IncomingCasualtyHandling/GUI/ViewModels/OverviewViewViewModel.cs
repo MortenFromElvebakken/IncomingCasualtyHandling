da098dd23d6e1cede532e0d997b31f86fff12ade
Anni Lykke Wilhelmsen <201509504@post.au.dk>
2018-11-07 15:40:50 +0100

    Cleaned up Viewmodel from stuff put in newly created Model

--- IncomingCasualtyHandling/GUI/ViewModels/OverviewViewViewModel.cs
+++ IncomingCasualtyHandling/GUI/ViewModels/OverviewViewViewModel.cs
@@ -36,6 +36,7 @@ namespace IncomingCasualtyHandling.GUI.ViewModels
         #region Specialties
 
         //private Specialty[] listOfSpecialities = new Specialty[16];
+        //private List<Specialty> specialties = new List<Specialty>();
 
         public Specialty Specialty1 => overviewModel.Specialty1;
 
@@ -69,8 +70,6 @@ namespace IncomingCasualtyHandling.GUI.ViewModels
 
         public Specialty Specialty16 => overviewModel.Specialty16;
 
-        private List<Specialty> specialties = new List<Specialty>();
-
         #endregion
 
 
@@ -94,83 +93,83 @@ namespace IncomingCasualtyHandling.GUI.ViewModels
         #region Constructor
         public OverviewViewViewModel()
         {
-            specialties.Add(new Specialty
-            {
-                Name = "Orthopaedic",
-                Colour = "#af3205",
-                Amount = 5,
-                ShowAs = Visibility.Visible
-            });
-            specialties.Add(new Specialty
-            {
-                Name = "Medicinal",
-                Colour = "#9400D3",
-                Amount = 9,
-                ShowAs = Visibility.Visible
-            });
-            specialties.Add(new Specialty
-            {
-                Name = "Thoracic surgery",
-                Colour = "#003865",
-                Amount = 4,
-                ShowAs = Visibility.Visible
-            });
+            //specialties.Add(new Specialty
+            //{
+            //    Name = "Orthopaedic",
+            //    Colour = "#af3205",
+            //    Amount = 5,
+            //    ShowAs = Visibility.Visible
+            //});
+            //specialties.Add(new Specialty
+            //{
+            //    Name = "Medicinal",
+            //    Colour = "#9400D3",
+            //    Amount = 9,
+            //    ShowAs = Visibility.Visible
+            //});
+            //specialties.Add(new Specialty
+            //{
+            //    Name = "Thoracic surgery",
+            //    Colour = "#003865",
+            //    Amount = 4,
+            //    ShowAs = Visibility.Visible
+            //});
 
-            var counter = 0;
-            Array.Clear(listOfSpecialities, 0, listOfSpecialities.Length);
-            specialties.Sort((x, y) => y.Amount.CompareTo(x.Amount));
-            foreach (var specialty in specialties)
-            {
-                if (specialty != null)
-                {
-                    listOfSpecialities[counter] = specialty;
-                    counter++;
-                }
-            }
+            //var counter = 0;
+            //Array.Clear(listOfSpecialities, 0, listOfSpecialities.Length);
+            //specialties.Sort((x, y) => y.Amount.CompareTo(x.Amount));
+            //foreach (var specialty in specialties)
+            //{
+            //    if (specialty != null)
+            //    {
+            //        listOfSpecialities[counter] = specialty;
+            //        counter++;
+            //    }
+            //}
 
-            _triage1 = new Triage
-            {
-                Amount = 8,
-                Colour = "#f60e0e",
-                Name = "Red",
-                ShowAs = Visibility.Visible
-            };
+            //_triage1 = new Triage
+            //{
+            //    Amount = 8,
+            //    Colour = "#f60e0e",
+            //    Name = "Red",
+            //    ShowAs = Visibility.Visible
+            //};
 
-            _triage2 = new Triage
-            {
-                Amount = 0,
-                Colour = "#f28d0e",
-                Name = "Orange",
-                ShowAs = Visibility.Collapsed
-            };
+            //_triage2 = new Triage
+            //{
+            //    Amount = 0,
+            //    Colour = "#f28d0e",
+            //    Name = "Orange",
+            //    ShowAs = Visibility.Collapsed
+            //};
 
-            _triage3 = new Triage
-            {
-                Amount = 5,
-                Colour = "#ffe913",
-                Name = "Yellow",
-                ShowAs = Visibility.Visible
-            };
-            _triage4 = new Triage
-            {
-                Amount = 2,
-                Colour = "#0bdd2e",
-                Name = "Green",
-                ShowAs = Visibility.Visible
-            };
-            _triage5 = new Triage
-            {
-                Amount = 0,
-                Colour = "#1e38ff",
-                Name = "Blue",
-                ShowAs = Visibility.Collapsed
-            };
+            //_triage3 = new Triage
+            //{
+            //    Amount = 5,
+            //    Colour = "#ffe913",
+            //    Name = "Yellow",
+            //    ShowAs = Visibility.Visible
+            //};
+            //_triage4 = new Triage
+            //{
+            //    Amount = 2,
+            //    Colour = "#0bdd2e",
+            //    Name = "Green",
+            //    ShowAs = Visibility.Visible
+            //};
+            //_triage5 = new Triage
+            //{
+            //    Amount = 0,
+            //    Colour = "#1e38ff",
+            //    Name = "Blue",
+            //    ShowAs = Visibility.Collapsed
+            //};
 
-            _eta = new ETA
-            {
-                AbsoluteTime = "10:42",
-                RelativeTime = "(-08:00)"
-            };
+            //_eta = new ETA
+            //{
+            //    AbsoluteTime = "10:42",
+            //    RelativeTime = "(-08:00)"
+            //};
         }
 
         #endregion
