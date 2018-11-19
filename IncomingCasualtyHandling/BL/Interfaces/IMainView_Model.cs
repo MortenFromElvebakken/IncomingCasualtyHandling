57fb5ac2293d0d739ad0cec59e6839e6d81df30e
Anni Lykke Wilhelmsen <201509504@post.au.dk>
2018-11-19 16:03:29 +0100

    Revert "Removed multiple interfaces frem merge"
    This reverts commit 1c5fa85702796b8565a4477eaf6b75a9b9635c39.

---
+++ IncomingCasualtyHandling/BL/Interfaces/IMainView_Model.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IncomingCasualtyHandling.BL.Object_classes;
+
+namespace IncomingCasualtyHandling.BL.Interfaces
+{
+    public interface IMainView_Model
+    {
+        List<Triage> ListOfTriages { get; set; }
+        string CurrentDateTime { get; set; }
+        Specialty Specialty1 { get; set; }
+        Triage Triage1 { get; set; }
+        Triage Triage2 { get; set; }
+        Triage Triage3 { get; set; }
+        Triage Triage4 { get; set; }
+        Triage Triage5 { get; set; }
+        ETA Eta { get; set; }
+        event PropertyChangedEventHandler PropertyChanged;
+    }
+}
