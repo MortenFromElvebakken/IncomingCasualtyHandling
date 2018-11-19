207b0288715a6ba9deb956ed186dbcd91c4ad240
Anni Lykke Wilhelmsen <201509504@post.au.dk>
2018-11-19 19:21:44 +0100

    SortingListOnETA made for simpler/less sorting in other classes

---
+++ IncomingCasualtyHandling/BL/Object classes/PatientListEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncomingCasualtyHandling.BL.Object_classes
+{
+    public class PatientListEventArgs
+    {
+        public PatientListEventArgs(List<PatientModel> sortedList)
+        {
+            this.SortedList = sortedList;
+        }
+
+        public List<PatientModel> SortedList { get; }
+    }
+}
