9ebc78755d0d026c7628636a217851ebbef8f3cb
Morten <mo.rt.en@hotmail.com>
2018-11-29 15:08:17 +0100

    small changes to names

--- IncomingCasualtyHandling/ObservableObject.cs
+++ IncomingCasualtyHandling/ObservableObject.cs
@@ -3,7 +3,7 @@ using System.Runtime.CompilerServices;
 using WindowTest2.Annotations;
 
 
-namespace IncomingCasualtyHandling.GUI.ViewModels
+namespace IncomingCasualtyHandling
 {
     public abstract class ObservableObject : INotifyPropertyChanged
     {
